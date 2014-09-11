823b23dd3b67b1413bf924236e50b44c709779b2
Krajcovic <dusan.krajcovic@gmail.com>
2014-09-11 15:31:28 +0200

    Changes to be committed: 	modified:   Procfile 	modified:   index.js

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello , this is app express framework!\n')
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
+app.set('port_01', ( parseInt(process.env.PORT)  + 1 || 5000 + 1))
+
 http.createServer(function(request, response) {
 	response.writeHead(200);
-	response.write("hello, this is a dog");
+	response.write("Hello, this is a http server\n");
 	response.end();
-}).listen(5001);
-
-console.log("Listening on port 5001....")
+}).listen(app.get('port_01'));
+console.log("Http server listening on port " + app.get('port_01') + "....");
